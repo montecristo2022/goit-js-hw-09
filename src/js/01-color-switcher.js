ee01ca64efacf808273d1ae62a5fc11e9c8dd59f
montecristo2022 <104501641+montecristo2022@users.noreply.github.com>
2022-08-07 12:18:05 +0300

    finished first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,39 @@
 
+
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const body = document.querySelector("body");
+startButton.addEventListener('click', newColor);
+stopButton.addEventListener('click', stopChangingColor);
+let id = null;
+
+
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function newColor() {
+    id = setInterval(() => {
+     const newColor = getRandomHexColor()
+
+ body.style.backgroundColor = newColor;
+    }, 1000)
+    
+    startButton.setAttribute('disabled', true)
+}
+
+function stopChangingColor() {
+    startButton.removeAttribute('disabled')
+    clearInterval(id)
+}
+
+
+
+
+
+
+
+
+
---
+++ src/js/02-timer.js
@@ -0,0 +1,35 @@
+// let id = null;
+
+// id = setInterval(() => {
+// const getSecondsToTomorrow = () => {
+//   const now = new Date();
+//   const tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
+
+//   const diff = tomorrow - now;
+
+//   return diff / 1000;
+//     };
+    
+
+// console.log(getSecondsToTomorrow());
+// }, 1000);
+ 
+
+// setTimeout(() => {
+// clearInterval(id)
+// }, 5000)
+
+
+
+
+
+// const id = setInterval(() => {
+//     console.log('setInterval 2000')
+// },);
+
+// console.log(id)
+
+
+// setTimeout(() => {
+// clearInterval(id)
+// }, 5000)
